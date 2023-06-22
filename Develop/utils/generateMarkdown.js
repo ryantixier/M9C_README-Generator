4aa53d0accc07cd85be56792159d8ce9a0836af2
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-06-21 18:25:23 -0600

    (GENERATEMARKDOWN.JS) removed superfluous text from Table of Contents.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -7,8 +7,6 @@ function generateMarkdown(data) {
   
   ## Table of Contents
   
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
   - [Installation](#installation)
   - [Usage](#usage)
   - [Credits](#credits)
